e303864fd3242d74a0cb3eccc16b718e4d8af151
bodhin1121 <89481452+bodhin1121@users.noreply.github.com>
2022-10-23 17:39:07 -0600

    Assignment 9

---
+++ Assignment 9/main.js
@@ -0,0 +1,45 @@
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const images = ['Snowboard.jpg', `Tetons.jpg`, `Nature.jpg`, `Steamboat.jpg`, `Aspens.jpg`];
+const alts = {
+  'Snowboard.jpg' : 'Snowboard Collage',
+  'Tetons.jpg' : 'Grand Teton National Park',
+  'Nature.jpg' : 'Nature Collage',
+  'Steamboat.jpg' : 'Steamboat Colorado at Sunset',
+  'Aspens.jpg' : 'Aspen trees in the fall'
+}
+
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
